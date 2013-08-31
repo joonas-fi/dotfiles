f644c9e3187987162841cf34bad78ec8d25994f7
Joonas Loppi <joonas@joonas.fi>
2013-08-31 14:57:33 +0300

    Check for pre-existing .bashrc before trying to symlink

--- install.sh
+++ install.sh
@@ -2,4 +2,12 @@
 #
 # $ curl -L http://go.xs.fi/dotfiles | sh
 
+if [ -f ~/.bashrc ]; then
+        echo "~/.bashrc already exists. Either:"
+        echo "a) dotfiles already installed or"
+        echo "b) there may be a stock .bashrc and you should mv ~/.bashrc ~/.bashrc.old"
+        exit 1 # error code
+fi
+
 ln -s ~/.dotfiles/bashrc ~/.bashrc
+
